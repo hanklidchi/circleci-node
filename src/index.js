77a77c1e13a3bc022499adae4ee4989c841d2f30
Henry Lidchi <henry@maze.design>
2021-03-02 09:45:34 +0200

    7

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-// const neo4j = require('neo4j-driver');
+const neo4j = require('neo4j-driver');
 
-// const driver = neo4j.driver(
-//   'bolt://localhost:7687',
-//   neo4j.auth.basic('neo4j', 'Senior Licensed Frozen Fish Terrace 52'),
-// );
-// const session = driver.session();
-// const personName = 'Alice';
+const driver = neo4j.driver(
+  'bolt://localhost:7687',
+  neo4j.auth.basic('neo4j', 'Senior Licensed Frozen Fish Terrace 52'),
+);
+const session = driver.session();
+const personName = 'Alice';
 
-// async function start() {
-//   try {
-//     const result = await session.run(
-//       'CREATE (a:Person {name: $name}) RETURN a',
-//       {
-//         name: personName,
-//       },
-//     );
+async function start() {
+  try {
+    const result = await session.run(
+      'CREATE (a:Person {name: $name}) RETURN a',
+      {
+        name: personName,
+      },
+    );
 
-//     const singleRecord = result.records[0];
-//     const node = singleRecord.get(0);
+    const singleRecord = result.records[0];
+    const node = singleRecord.get(0);
 
-//     console.log(node.properties.name);
-//   } finally {
-//     await session.close();
-//   }
+    console.log(node.properties.name);
+  } finally {
+    await session.close();
+  }
 
-//   // on application exit:
-//   await driver.close();
-// }
+  // on application exit:
+  await driver.close();
+}
 
-// start();
+start();
 
 console.log('hello');
