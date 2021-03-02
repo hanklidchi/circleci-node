7e4b64e94cff26ed5c47b104aeade9ed91fb4f86
Henry Lidchi <henry@maze.design>
2021-03-02 12:34:49 +0200

    24

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const neo4j = require('neo4j-driver');
 
 const driver = neo4j.driver(
-  'bolt://127.0.0.1:7687',
+  'http://localhost:7474',
   // 'neo4j://localhost',
   // neo4j.auth.basic('neo4j', 'password'),
 );
