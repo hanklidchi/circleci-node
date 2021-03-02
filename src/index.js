b39aa749fdb621d651dafdcf597b04ed6c0b38c7
Henry Lidchi <henry@maze.design>
2021-03-02 09:39:52 +0200

    5

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// const neo4j = require('neo4j-driver');
+
+// const driver = neo4j.driver(
+//   'bolt://localhost:7687',
+//   neo4j.auth.basic('neo4j', 'Senior Licensed Frozen Fish Terrace 52'),
+// );
+// const session = driver.session();
+// const personName = 'Alice';
+
+// async function start() {
+//   try {
+//     const result = await session.run(
+//       'CREATE (a:Person {name: $name}) RETURN a',
+//       {
+//         name: personName,
+//       },
+//     );
+
+//     const singleRecord = result.records[0];
+//     const node = singleRecord.get(0);
+
+//     console.log(node.properties.name);
+//   } finally {
+//     await session.close();
+//   }
+
+//   // on application exit:
+//   await driver.close();
+// }
+
+// start();
+
+console.log('hello');
